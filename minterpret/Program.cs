cff6efd3a1a3680b681c74649e7b6f0da026b27d
Sunny Gulati <sunnygulati@kenmarkeyewear.com>
2023-06-16 20:31:30 -0400

    initial stab - reading file

---
+++ minterpret/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CsvHelper.Configuration;
+using CsvHelper;
+using System.Globalization;
+
+var pathToInputFile = @"C:\Users\sgulati\OneDrive\2023P\mint_transactions.csv";
+
+var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+    PrepareHeaderForMatch = args => args.Header.Replace(" ",""),
+};
+using var reader = new StreamReader(pathToInputFile);
+using var csv = new CsvReader(reader, config);
+var records = csv.GetRecords<MintRecord>().ToList();
+
+Console.WriteLine($"Loaded {records.Count} records");
