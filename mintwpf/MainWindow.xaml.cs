582dd61f2f32eb49784f9de611a5b23ca262ac2d
Sunny Gulati <sunnygulati@kenmarkeyewear.com>
2023-06-30 22:34:22 -0400

    i have a fricking chart.
    had to go back to .net framework 4.8 but i have it.  But this library seems to support newer verisons too

---
+++ mintwpf/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.DataVisualization.Charting;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mintwpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var c = new Chart();
+            var series = new AreaSeries() { Title = "Area1", IndependentValuePath = "Key", DependentValuePath = "Value"};
+            series.ItemsSource = new KeyValuePair<string, int>[]
+            {
+                new KeyValuePair<string, int>("A", 5),
+                new KeyValuePair<string, int>("B", 6),
+                new KeyValuePair<string, int>("C", 7)
+            };
+            c.Series.Add(series); 
+            
+            g.Children.Add(c);
+        }
+    }
+}
