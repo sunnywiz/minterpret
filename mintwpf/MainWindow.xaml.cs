ebc58d1fc99f221dd259eea7437a5662cbb1c767
Sunny Gulati <sunnygulati@kenmarkeyewear.com>
2023-08-29 15:58:20 -0400

    Reading files but flagging dupes

--- mintwpf/MainWindow.xaml.cs
+++ mintwpf/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,81 +16,125 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CsvHelper;
+using CsvHelper.Configuration;
 using Microsoft.Win32;
 
-namespace mintwpf
+namespace mintwpf;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+
+        CheckGuards(); 
+
+        var c = new Chart();
+
+        var sas = new StackedAreaSeries();
+        sas.SeriesDefinitions.Add(new SeriesDefinition()
         {
-            InitializeComponent();
-
-            CheckGuards(); 
-
-            var c = new Chart();
-
-            var sas = new StackedAreaSeries();
-            sas.SeriesDefinitions.Add(new SeriesDefinition()
+            Name="A2", 
+            IndependentValuePath = "Key",
+            DependentValuePath = "Value",
+            ItemsSource = new[]
             {
-                Name="A2", 
-                IndependentValuePath = "Key",
-                DependentValuePath = "Value",
-                ItemsSource = new[]
-                {
-                    new KeyValuePair<DateTime, int>(DateTime.Now.Date.AddDays(-30), 5),
-                    new KeyValuePair<DateTime, int>(DateTime.Now.Date.AddDays(-20), 6),
-                    new KeyValuePair<DateTime, int>(DateTime.Now.Date.AddDays(-10), 8)
-                },
-            });
-            sas.SeriesDefinitions.Add(new SeriesDefinition()
-            {
-                Name = "A1",
-                IndependentValuePath = "Key",
-                DependentValuePath = "Value",
-                ItemsSource = new[]
+                new KeyValuePair<DateTime, int>(DateTime.Now.Date.AddDays(-30), 5),
+                new KeyValuePair<DateTime, int>(DateTime.Now.Date.AddDays(-20), 6),
+                new KeyValuePair<DateTime, int>(DateTime.Now.Date.AddDays(-10), 8)
+            },
+        });
+        sas.SeriesDefinitions.Add(new SeriesDefinition()
+        {
+            Name = "A1",
+            IndependentValuePath = "Key",
+            DependentValuePath = "Value",
+            ItemsSource = new[]
             {
                 new KeyValuePair<DateTime, int>(DateTime.Now.Date.AddDays(-30), 4),
                 new KeyValuePair<DateTime, int>(DateTime.Now.Date.AddDays(-20), 3),
                 new KeyValuePair<DateTime, int>(DateTime.Now.Date.AddDays(-10), 0)
             }
-            });
-            sas.Name = "Boogie";
-            c.Series.Add(sas);
-            GroupBoxGraphResult.Content=c;
-        }
+        });
+        sas.Name = "Boogie";
+        c.Series.Add(sas);
+        GroupBoxGraphResult.Content=c;
+    }
 
-        public void CheckGuards()
-        {
-            ButtonLoadMintCsv.IsEnabled = !String.IsNullOrWhiteSpace(TextBoxMintCsvDirectory.Text) &&
-                                          Directory.Exists(TextBoxMintCsvDirectory.Text);
-
-        }
-
-        private void ButtonChooseMintCsvDirectory_Click(object sender, RoutedEventArgs e)
-        {
-            var ofd = new SaveFileDialog()
-            {
-                InitialDirectory = TextBoxMintCsvDirectory.Text,
-                FileName = "Choose folder filename doesnt matter",
-                CheckPathExists = true,
-                DefaultExt = ".csv"
-            };
-            if (ofd.ShowDialog() == true)
-            {
-                TextBoxMintCsvDirectory.Text = System.IO.Path.GetDirectoryName(ofd?.FileName)??string.Empty;
-            }
-
-            CheckGuards(); 
-        }
-
-        private void ButtonLoadMintCsv_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+    public void CheckGuards()
+    {
+        ButtonLoadMintCsv.IsEnabled = !String.IsNullOrWhiteSpace(TextBoxMintCsvDirectory.Text) &&
+                                      Directory.Exists(TextBoxMintCsvDirectory.Text);
 
     }
+
+    private void ButtonChooseMintCsvDirectory_Click(object sender, RoutedEventArgs e)
+    {
+        var ofd = new SaveFileDialog()
+        {
+            InitialDirectory = TextBoxMintCsvDirectory.Text,
+            FileName = "Choose folder filename doesnt matter",
+            CheckPathExists = true,
+            DefaultExt = ".csv"
+        };
+        if (ofd.ShowDialog() == true)
+        {
+            TextBoxMintCsvDirectory.Text = System.IO.Path.GetDirectoryName(ofd?.FileName)??string.Empty;
+        }
+
+        CheckGuards(); 
+    }
+
+    private void ButtonLoadMintCsv_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            var di = new DirectoryInfo(TextBoxMintCsvDirectory.Text);
+            if (!di.Exists) return;
+
+            var files = di.GetFiles("*.csv").OrderByDescending(x => x.Name).ToList();
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                PrepareHeaderForMatch = args => args.Header.Replace(" ", ""),
+            };
+            List<MintRecord> allRecords = new List<MintRecord>();
+            foreach (var file in files)
+            {
+                List<MintRecord> records;
+                using (var reader = new StreamReader(file.FullName))
+                {
+                    using var csv = new CsvReader(reader, config);
+                    records = csv.GetRecords<MintRecord>().ToList();
+                    var byGroup = records.GroupBy(x => new
+                        { x.Date, x.OriginalDescription, x.Amount, x.TransactionType, x.AccountName });
+                    foreach (var g in byGroup)
+                    {
+                        if (g.Count() > 1)
+                        {
+                            throw new NotSupportedException(
+                                $"Date/Description/Amount/TransactionType/AccountName not unique in file {file.Name}");
+                        }
+                    }
+                }
+
+                TextBoxLoadResult1.Text = $"Loaded {records.Count} records from {file.Name}"; 
+            }
+        }
+        catch (Exception ex)
+        {
+            HandleException(ex); 
+        }
+    }
+
+    public void HandleException(Exception ex)
+    {
+        TextBoxErrors.Text = ex.ToString();
+        TextBoxErrors.Foreground = new SolidColorBrush(Colors.Red); 
+    }
+
 }
---
+++ mintwpf/MintRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+public class MintRecord
+{
+    public DateTime Date { get; set; }
+    public string Description { get; set; }
+    public string OriginalDescription { get; set; }
+    public decimal Amount { get; set; }
+    public string TransactionType { get; set; }
+    public string Category { get; set; }
+    public string AccountName { get; set; }
+    public string Labels { get; set; }
+    public string Notes { get; set; }
+
+    public decimal SignedAmount => TransactionType == "credit" ? Amount : -Amount; 
+
+}
